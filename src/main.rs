a7b9ce381feec6ea9eb8518b61dd6b3d7b989ea8
rrokkam <rohithrokkam@gmail.com>
2020-04-26 10:25:45 -0700

    Add can_hold and associated function to create a square

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width >= other.width && self.height >= other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -8,9 +26,17 @@ fn main() {
         width: 30,
         height: 50,
     };
-    println!("Area = {}", area(&rect));
+    println!("Rect is {:#?}", rect);
+    println!("Rect's area = {}", rect.area());
+
+    let square = Rectangle::square(30);
+
+    let another_rect = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("can rect hold square? {}", rect.can_hold(&square));
+    println!("can rect hold another_rect? {}", rect.can_hold(&another_rect));
 }
 
-fn area(rect: &Rectangle) -> u32 {
-    rect.width * rect.height
-}
