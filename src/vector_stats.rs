9840b504acb8e162535d0f4adc2f2d1d9ee5d7a4
rrokkam <rohithrokkam@gmail.com>
2020-04-30 22:33:19 -0700

    Make helper functions private to impl block

--- src/vector_stats.rs
+++ src/vector_stats.rs
@@ -10,29 +10,29 @@ pub struct Stats {
 impl Stats {
     pub fn compute_stats(array: &[i32]) -> Self {
         Stats {
-            mean: mean(array),
-            median: median(array),
-            mode: mode(array),
+            mean: Stats::mean(array),
+            median: Stats::median(array),
+            mode: Stats::mode(array),
         }
     }
-}
 
-pub fn mean(array: &[i32]) -> f64 {
-    let sum: i32 = array.iter().sum();
-    sum as f64 / array.len() as f64
-}
-
-pub fn median(array: &[i32]) -> i32 {
-    array[0]
-}
-
-pub fn mode(array: &[i32]) -> i32 {
-    let mut map: HashMap<i32, u32> = HashMap::new();
-    for elem in array.iter() {
-        *map.entry(*elem).or_insert(0) += 1;
+    fn mean(array: &[i32]) -> f64 {
+        let sum: i32 = array.iter().sum();
+        sum as f64 / array.len() as f64
     }
-    map.into_iter()
-        .max_by_key(|&(_, c)| c)
-        .map(|(elem, _)| elem)
-        .unwrap()
+
+    fn median(array: &[i32]) -> i32 {
+        array[0]
+    }
+
+    fn mode(array: &[i32]) -> i32 {
+        let mut map: HashMap<i32, u32> = HashMap::new();
+        for elem in array.iter() {
+            *map.entry(*elem).or_insert(0) += 1;
+        }
+        map.into_iter()
+            .max_by_key(|&(_, c)| c)
+            .map(|(elem, _)| elem)
+            .unwrap()
+    }
 }
