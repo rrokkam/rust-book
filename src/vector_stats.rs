02f2931e4025c82ff911baa54e2780907f1f0db0
rrokkam <rohithrokkam@gmail.com>
2020-04-30 22:25:41 -0700

    Use max_by_key for mode

--- src/vector_stats.rs
+++ src/vector_stats.rs
@@ -23,7 +23,7 @@ pub fn mean(array: &[i32]) -> f64 {
 }
 
 pub fn median(array: &[i32]) -> i32 {
-    array[3]
+    array[0]
 }
 
 pub fn mode(array: &[i32]) -> i32 {
@@ -31,13 +31,5 @@ pub fn mode(array: &[i32]) -> i32 {
     for elem in array.iter() {
         *map.entry(*elem).or_insert(0) += 1;
     }
-    map.iter()
-        .fold((0 as i32, 0 as u32), |acc, x| {
-            if *x.1 > acc.1 {
-                (*x.0, *x.1)
-            } else {
-                acc
-            }
-        })
-        .0
+    map.into_iter().max_by_key(|&(_, c)| c).unwrap().0
 }
