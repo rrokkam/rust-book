f9f6c723540ef69113c39b272d5d8b6a00ff7525
rrokkam <rohithrokkam@gmail.com>
2020-05-01 22:41:36 -0700

    Stub out piglatin

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@
 
 mod vector_stats;
 use vector_stats::Stats;
-// use pig_latin;
+
+mod pig_latin;
+use pig_latin::pig_latin;
 // use department_manager;
 
 fn main() {
+    test_vector_stats();
+    test_pig_latin();
+}
+
+fn test_vector_stats() {
     let array = [20, 2, 234, 235, 5, 3, 4, 4, 6, 7, 2342];
     let stats = Stats::compute_stats(&array);
     println!("vector::stats for array {:?}", array);
@@ -13,4 +20,9 @@ fn main() {
         "mean: {}, median: {}, mode: {}",
         stats.mean, stats.median, stats.mode
     );
+}
+
+fn test_pig_latin() {
+    let text1 = "arstdhneio";
+    println!("{} piglatinized is {}", text1, pig_latin(text1));
 }
---
+++ src/pig_latin.rs
@@ -0,0 +1,3 @@
+pub fn pig_latin(input: &str) -> &str {
+    input
+}
