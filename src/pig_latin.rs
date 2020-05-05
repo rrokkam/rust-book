d3c205130d2607569e5ac627e7d3ddc5726d6f56
rrokkam <rohithrokkam@gmail.com>
2020-05-04 19:38:22 -0700

    Implement pig latin

--- src/pig_latin.rs
+++ src/pig_latin.rs
@@ -1,3 +1,6 @@
-pub fn pig_latin(input: &str) -> &str {
-    input
+pub fn pig_latin(input: &str) -> String {
+    let mut indices = input.char_indices();
+    let first_char = indices.next().expect("Can't convert empty string to pig latin").1;
+    let rest = indices.as_str();
+    format!("{}{}ay", rest, first_char)
 }
