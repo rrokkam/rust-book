8eaf9a65c6944553070ebb33d51ed97e4b0d4a2d
rrokkam <rohithrokkam@gmail.com>
2020-05-05 17:25:33 -0700

    Parse args using struct Config

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
-use std::env;
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Search for {} in file {}", query, filename);
+    let contents = fs::read_to_string(config.filename).expect("Couldn't read the file.");
+    println!("{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments. Need query and filename.");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
