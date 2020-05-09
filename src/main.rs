392e28ca30bc62320e9493296725d29ba7702349
rrokkam <rohithrokkam@gmail.com>
2020-05-09 08:13:36 -0700

    Print out received requests

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
-        println!("Connection established.");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    println!("Connection established.");
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
