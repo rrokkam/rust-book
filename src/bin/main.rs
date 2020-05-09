c00e1070986e49485937bac7da393f24f398e1a3
rrokkam <rohithrokkam@gmail.com>
2020-05-09 09:29:26 -0700

    Use PoolCreationError type instead of asserting on positive size.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,7 +7,7 @@ use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let pool = ThreadPool::new(4).unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+use std::error;
+use std::fmt;
+
+#[derive(Debug)]
+pub struct PoolCreationError {
+    msg: &'static str,
+}
+
+impl error::Error for PoolCreationError {}
+
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Pool creation error: {}", self.msg)
+    }
+}
+
 pub struct ThreadPool;
 
 impl ThreadPool {
@@ -6,9 +22,13 @@ impl ThreadPool {
     /// # Panics
     ///
     /// The `new` function will panic if the size is zero.
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
-        ThreadPool
+    pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        match size {
+            0 => Err(PoolCreationError {
+                msg: "Can't create an empty ThreadPool.",
+            }),
+            _ => Ok(ThreadPool),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
