74937fdcf475eaf466dc206f4eca6d88f01fc42e
rrokkam <rohithrokkam@gmail.com>
2020-05-09 08:35:50 -0700

    Respond with Hello from Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
@@ -11,10 +12,12 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
