8414fd736125093b1f2f7c72dcadd83cbda6955f
rrokkam <rohithrokkam@gmail.com>
2020-05-09 18:35:25 -0700

    Use Worker and Job to concurrently handle requests

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 use std::error;
 use std::fmt;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
 
 #[derive(Debug)]
 pub struct PoolCreationError {
@@ -14,7 +18,12 @@ impl fmt::Display for PoolCreationError {
     }
 }
 
-pub struct ThreadPool;
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+pub struct ThreadPool {
+    sender: mpsc::Sender<Job>,
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
     /// Make a new ThreadPool with `size` threads.
@@ -23,17 +32,49 @@ impl ThreadPool {
     ///
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
-        match size {
-            0 => Err(PoolCreationError {
+        if size == 0 {
+            return Err(PoolCreationError {
                 msg: "Can't create an empty ThreadPool.",
-            }),
-            _ => Ok(ThreadPool),
+            });
         }
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+        for id in 0..size {
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        Ok(ThreadPool { sender, workers })
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver
+                .lock()
+                .expect("Receiver lock was poisoned")
+                .recv()
+                .expect("Job sender shut down");
+            println!("Worker {} got a job; executing.", id);
+            job();
+        });
+
+        Worker { id, thread }
     }
 }
